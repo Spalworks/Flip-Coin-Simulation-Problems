a9cb1cf19e45dde528d99d04e04028ae29f56c8e
spalworks <spalworks@gmail.com>
2022-12-08 00:34:20 +0530

    [Sourav]-Multiple flip committed successfully

---
+++ UC2multipleFlip.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for (( i=0; i<10; i++ ))
+do
+        coin=$((RANDOM%2))
+
+        if [ $coin -eq 0 ]; then
+                echo "Head"
+        else
+                echo "Tail"
+        fi
+done
