0d9fb6de1b37fa903ec9964c38e1ad10aaf54493
spalworks <spalworks@gmail.com>
2022-12-08 00:11:05 +0530

    [Sourav]-Flipping coin commit Successful

---
+++ UC1flipcoinSimulator.sh
@@ -0,0 +1,9 @@
+coin=$((RANDOM%2))
+
+        if [ $coin -eq 0 ]; then
+                echo "Head"
+	          (( count1++ ))
+        else
+                echo "Tail"
+		    (( count2++ ))
+        fi
